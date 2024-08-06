a0c8d2e23b2e665faf63dcb7c0c76c304d9ed352
Yubraj bhandari <30431939+yubrajbhandari923@users.noreply.github.com>
2024-08-06 23:58:29 +0200

    Uploaded fastapi server and index.html

---
+++ tsp_server.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, Form
+
+from typing import Annotated
+from fastapi.middleware.cors import CORSMiddleware
+
+"""
+Installations:
+pip install fastapi uvicorn python-multipart
+
+Run:
+uvicorn tsp_server:app --reload
+
+"""
+
+def tsp_quantum_solver(adj_matrix):
+    return [0, 1, 2, 3]
+
+distances_matrix = [
+    [0, 1, 2, 3],
+    [1, 0, 4, 5],
+    [2, 4, 0, 6],
+    [3, 5, 6, 0]
+]
+
+
+
+
+def get_adj_matrix(list_of_cities):
+    adj_matrix = []
+    for i in range(len(list_of_cities)):
+        adj_matrix.append([])
+        for j in range(len(list_of_cities)):
+            adj_matrix[i].append(distances_matrix[list_of_cities[i]][list_of_cities[j]])
+    return adj_matrix
+
+app = FastAPI()
+
+# Allow cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/solve")
+def solve_tsp(nodes: Annotated[str, Form(...)]):
+    nodes = list(map(int, nodes.split(',')))
+    print(nodes, type(nodes))
+    
+    adj_matrix = get_adj_matrix(nodes)
+    print(adj_matrix)
+    return tsp_quantum_solver(adj_matrix)
